b13b8cce6d5a5cdc739bef7e1b71e8c9612706e4
Kirill <kkirilenkov@gmail.com>
2019-10-14 01:37:00 +0700

    with my path setter

--- nback_analyzer.py
+++ nback_analyzer.py
@@ -1,12 +1,25 @@
 import pandas as pd
 import os
 
-class NoMatching(Exception):
+class NoMatch(Exception):
     pass
 
-# referenceDir = input('Введите полный путь к папке, в которой находятся логфайлы. Стиль ввода X:/example:\n')
-referenceDir = 'C:/Users/Kirill/.virtualenvs/ML/average_of_nBack/logs'
-os.chdir(referenceDir)
+default_msg = 'Enter the full path to the folder with the log files: \n'
+
+def path_setter(link, message=default_msg, stage=False):
+    if message[-1] != '\n':
+        message += '\n'
+    try:
+        os.chdir(link)
+    except FileNotFoundError:
+        if not stage:
+            print('Hard path not found')
+        else:
+            print('Cannot find the specified path')
+        path_setter(input(message), message=message, stage=True)
+
+Dir = 'C:/Users/Kirill/.virtualenvs/EEG_projects/nBack/logs/'
+path_setter(Dir)
 allFiles = os.listdir(path='.')
 logNames = list(filter(lambda x : x[0] != '_', allFiles))
 responsesLogs = list(filter(lambda x : x[0] == '_', allFiles))
@@ -30,9 +43,9 @@ def log_analyzer(log):
             #print("step {0:d}, value {1:.2f}".format(i, df.loc[i, "Duration"]), end='\n')
             blockDiff += df.loc[i, "Duration"]
             j += 1
-        if j == 14:
-            diff = (blockDiff - 140000)/10
-            print("block: {0: d}, diff: {1:.1f}, frames: {2:.2f}".format(block, diff, diff/16.7), end="\n")
+        if j == 28:
+            diff = (blockDiff - 420000)/10
+            print("block: {0: d}, diff [ms]: {1:.1f}, frames: {2:.2f}".format(block, diff, diff/16.7), end="\n")
             j = 0
             blockDiff = 0
             block += 1
@@ -51,8 +64,8 @@ def responses(log):
                 file_report(r)
                 i = not i
         if i:
-            raise NoMatching
-    except NoMatching:
+            raise NoMatch
+    except NoMatch:
         print("Нет файла статистики ответов для " + log)
     except FileNotFoundError as e:
         print(e)
